c90e5ee94d9d4026e33e41ed89d733461a541c43
Kubanych-test <zhkk2004@gmail.com>
2021-05-05 18:13:48 +0600

    Debuged version - First commte

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--4kl2^t@n&o87u$6+zp&+&6b8y7imr+e++!_)yo5*dk+pxh1&$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
